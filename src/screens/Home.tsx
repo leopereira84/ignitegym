6ccf782ac0dc9ea7fdc093c86dca270fc163a83c
Leonardo Andrey Pereira <leonardo.pereira@itajai.sc.gov.br>
2024-01-17 17:08:02 -0300

    Rotas Privadas e Montando a Home

---
+++ src/screens/Home.tsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import { VStack, FlatList, HStack, Heading, Text } from "native-base";
+
+import { HomeHeader } from "@components/HomeHeader";
+import { Group } from "@components/Group";
+import { ExerciseCard } from "@components/ExerciseCard";
+
+export function Home() {
+  const [groups, setGroups] = useState(["costas", "bíceps", "tríceps", "ombros"]);
+  const [groupSelected, setGroupSelected] = useState("costas");
+
+  return (
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+
+        <ExerciseCard />
+        <ExerciseCard />
+      </VStack>
+
+    </VStack>
+  )
+}
